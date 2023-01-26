e1e3ff15f0ad9eec2bb798e3440ba81205638f7a
Balloon208 <2022035@bssm.hs.kr>
2023-01-26 13:52:03 +0900

    using event in react

--- reacts/src/App.js
+++ reacts/src/App.js
@@ -9,7 +9,8 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
+      selected_content_id:2,
       subject:{title:"WEB", sub:"world wide web!"},
       welcome:{title:'Welcome', desc:"Hello React!"},
       contents:[
@@ -26,14 +27,37 @@ class App extends Component{
       _desc = this.state.welcome.desc;
     }
     else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      let i=0;
+
+      while(i < this.state.contents.length){
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++;
+      }
+      
     }
     return(
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <Subject title="React" sub="for UI"></Subject>
-        <TOC data={this.state.contents}></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
+        data={this.state.contents}>
+        </TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- reacts/src/components/TOC.js
+++ reacts/src/components/TOC.js
@@ -7,7 +7,20 @@ class TOC extends Component{
       let i = 0;
       while(i < data.length)
       {
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+
+        <li key={data[i].id}>
+          <a
+          href={"/content/"+data[i].id}
+          // data-id={data[i].id} 로도 가능
+          onClick={function(id, e){
+            e.preventDefault();
+            this.props.onChangePage(id);
+            // data-id 이용시 this.props.onChangePage(e.target.dataset.id);
+          }.bind(this, data[i].id)}
+          >{data[i].title}</a>
+        </li>);
+
         i++;
       }
       return(
